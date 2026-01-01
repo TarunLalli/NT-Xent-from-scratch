df57795186cfb875b67942fa3810f4553eeeabce
TarunLalli <tarunslalli@outlook.com>
2026-01-01 17:48:41 +0000

    First block of code: Adding l2 norm, similarity matrix positive similarity extraction Masking
    Next block:
    temperature scaling
    logit calcs
    final loss calc

--- main.py
+++ main.py
@@ -5,5 +5,32 @@ class NTXent(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def lossfunction(self):
-        ...
+    def forward(self, z1, z2):
+        self.B = z1.shape[0]
+        z = torch.cat([self.l2norm(z1),self.l2norm(z2)],dim=0)
+        
+        S, positives = self.similarityMatrix(z)
+
+    def l2norm(self, x):
+        return torch.nn.functional.normalize(x, p=2, dim=1)
+
+    def similarityMatrix(self,z):
+        S = z @ z.T
+        rows = [row for row in range(2*self.B)]
+        cols = []
+        for row in rows:
+            if row <= (self.B-1):
+                cols.append(row+self.B)
+            elif row > (self.B-1):
+                cols.append(row-self.B)
+        positives = S[rows,cols]
+
+        return S, positives
+    
+    def masking(self, S):
+        mask = torch.eye(2*self.B, dtype=torch.bool).to(S.device)
+        S = S.masked_fill_(mask=mask, value=float('-inf'))
+        
+        return S
+
+
